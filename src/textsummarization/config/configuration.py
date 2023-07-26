cdb0be77553e52149a365f973fc1c91823278d9f
SahilChowkekar <chowkekar95@gmail.com>
2023-07-26 13:46:12 -0700

    data validation added

--- src/textsummarization/config/configuration.py
+++ src/textsummarization/config/configuration.py
@@ -1,6 +1,6 @@
 from textsummarization.constants import *
 from textsummarization.utils.common import read_yaml, create_directories
-from textsummarization.entity import (DataIngestionConfig)
+from textsummarization.entity import (DataIngestionConfig,DataValidationConfig)
 
 
 class ConfigurationManager:
@@ -27,4 +27,18 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir 
         )
 
-        return data_ingestion_config    
+        return data_ingestion_config  
+
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config  
